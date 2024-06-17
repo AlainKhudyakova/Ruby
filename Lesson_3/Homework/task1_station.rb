e4b7cb1f00171a1f4f545d788d7bb5d809d932d8
AlainKhudyakova <khudyakova.alain@gmail.com>
2024-06-17 21:04:47 +0300

    Lesson 3

--- Lesson_3/Homework/task1_station.rb
+++ Lesson_3/Homework/task1_station.rb
@@ -9,15 +9,18 @@ class Station
    def arrive(train)
       @trains << train
       end
-   
+
    def return_type(type)
       @trains.delete(train)
    end
+
    def show_trains_by_type(type)
       @trains.select{|train| train.type == type}
       puts "#{@trains}"
    end
+
 end
+
 
 class Route
    attr_reader :route, :stations
@@ -27,68 +30,87 @@ def initialize(initial_point, final_point)
    @final_point = final_point
    @stations = [initial_point, final_point]
 end
+
 def add_station(station)
    @stations.inspect(-2, station)
 end
+
 def delete_station(station)
    @station.delete(station)
 end
+
 def show_stations
    @stations
 end
+
 end
 
 class Train
    attr_reader :number, :type
+
    def initialize(number, type)
       @number = number
       @type = type
    end
+
    def start
       @speed = 15
    end
+
    def spped up
       @speed += 15
    end
+
    def current_speed
       @speed
    end
+
    def speed_down
       @speed -= 15 if @speed > 0
    end
+
    def add_wagons
       @wagons = 0
       @wagons - @wagons + 1 if @speed == 0
    end
+
    def del_wagons
       @wagons -= 1 if @wagons >= 0 || @speed == 0
    end
+
    def show_wagons
       @wagons
    end
+
    def s_route(route)
       @route = route
       @current_station_index = 0
    end
+
    def show_train_route
       @route.stations
    end
+
    def move_forward
       @current_station_index += 1 if next_station
    end
+
    def move_back
       @current_station_index -= 1 if previous_station
    end
+
    def current_station
       @route.stations[@current_station_index]
    end
+
    def next_station
       @route.stations[@current_station_index + 1]
    end
+
    def previous_station
       @route.stations[@current_station_index - 1]
    end
-   
+
    def location
       puts"previous: #{@route.stations[@current_station_index - 1]}"
       puts"current: #{@route.stations[@current_station_index]}"
