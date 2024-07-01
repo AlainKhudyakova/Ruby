b03957821177b8ff5f4b9d62c0495269aa74e904
AlainKhudyakova <khudyakova.alain@gmail.com>
2024-07-01 20:57:02 +0300

    Lesson 4

--- Lesson_4/Homework/Main.rb
+++ Lesson_4/Homework/Main.rb
@@ -18,9 +18,9 @@ class RailRoad
 
  def start
   loop do
-   show_menu
-   command = gets.chomp.to_i
-   run(command)
+  show_menu
+  command = gets.chomp.to_i
+  run(command)
   end
  end
 
@@ -42,7 +42,7 @@ class RailRoad
   puts "12. Close menu"
  end
 
- def run(command)
+def run(command)
   case command
   when 1 then create_station
   when 2 then create_train
@@ -57,102 +57,102 @@ class RailRoad
   when 11 then show_trains_on_stations
   when 12 then exit
   else
-   puts "there is no such command, try again" 
+  puts "there is no such command, try again" 
   end
  end
 end
 
 #1
 def create_station
- puts "Create a station:"
+  puts "Create a station:"
   name = gets.chomp
-   @stations << Station.new(name)
- puts "Station \"#{name}\" created"
- puts "List of stations"
-   @stations.each_with_index {|val, index| puts "#{index + 1}. #{val.name}" }
+  @stations << Station.new(name)
+  puts "Station \"#{name}\" created"
+  puts "List of stations"
+  @stations.each_with_index {|val, index| puts "#{index + 1}. #{val.name}" }
 end
 
 #2
 def create_train
- puts "Create number a train:"
+  puts "Create number a train:"
   number = gets.chomp
- puts "Enter type train : cargo or passanger"
+  puts "Enter type train : cargo or passanger"
   type = gets.chomp.capitalize
 if type == "Cargo"
-   @trains << TrainCargo.new(number)
- puts "Train \"#{type}\" number : \"#{number}\" created"
+  @trains << TrainCargo.new(number)
+  puts "Train \"#{type}\" number : \"#{number}\" created"
 elsif type == "Passanger"
-   @trains << TrainPassanger.new(number)
- puts "Train \"#{type}\" number : \"#{number}\" created"
+  @trains << TrainPassanger.new(number)
+  puts "Train \"#{type}\" number : \"#{number}\" created"
 else
- puts "Enter the required train type"
+  puts "Enter the required train type"
 end
-   @trains.each_with_index {|val, index| puts "#{index + 1}. #{val.number} - #{val.type} "}
+  @trains.each_with_index {|val, index| puts "#{index + 1}. #{val.number} - #{val.type} "}
 end
 
 #3
 def create_route
- puts "initial_point:"
-   @initial_point = station_select
- puts "final_point:"
-   @final_point = station_select
-   @routes << Route.new(@initial_point, @final_point)
- puts "Route #{@initial_point.name} - #{@final_point.name} created"
+  puts "initial_point:"
+  @initial_point = station_select
+  puts "final_point:"
+  @final_point = station_select
+  @routes << Route.new(@initial_point, @final_point)
+  puts "Route #{@initial_point.name} - #{@final_point.name} created"
 end
 
 #4
 def create_wagon
-   @wagons << WagonCargo.new
-   @wagons << WagonPassanger.new
- puts @wagons
-   @wagons.each_with_index {|wagon, index| puts "#{index + 1}.#{wagon.type}"}
+  @wagons << WagonCargo.new
+  @wagons << WagonPassanger.new
+  puts @wagons
+  @wagons.each_with_index {|wagon, index| puts "#{index + 1}.#{wagon.type}"}
 end
 
 #5
 def create_station_route
- route = route_select
- station = station_select
- route.add_station(station)
- route.show_route_stations
+  route = route_select
+  station = station_select
+  route.add_station(station)
+  route.show_route_stations
 end
 
 #6
 def delete_station_route
- route = route_select
- station = station_select
- route.delete_station_route(station)
- route.show_route_stations
+  route = route_select
+  station = station_select
+  route.delete_station_route(station)
+  route.show_route_stations
 end
 
 #7
 def set_route
- train = train_select
- route = route_select
- train.set_route(route)
+  train = train_select
+  route = route_select
+  train.set_route(route)
   @initial_point.train_arrival(train)
- train.show_train_route
- train.show_current_station
+  train.show_train_route
+  train.show_current_station
 end
 
 #8
 def add_wagons
- train = train_select
- train.add_wagons
- train.show_wagons
+  train = train_select
+  train.add_wagons
+  train.show_wagons
 end
 
 #9
 def delete_wagons
- train = train_select
- train.delete_wagons
- train.show_wagons
+  train = train_select
+  train.delete_wagons
+  train.show_wagons
 end
 
 #10
 def move_train
- train = train_select
- puts "Train is heading: forward(f) or back(b)?"
- direction = gets.chomp
+  train = train_select
+  puts "Train is heading: forward(f) or back(b)?"
+  direction = gets.chomp
 
  if direction == "f"
   train.current_station.departure(train)
@@ -172,42 +172,42 @@ end
 
 #11
 def show_trains_on_stations
- @stations.each_with_index do|station, index| puts "#{station.name}"
+  @stations.each_with_index do|station, index| puts "#{station.name}"
   station.trains.select do|train|
-   print " #{train.number} - #{train.type};"
-   puts ""
+  print " #{train.number} - #{train.type};"
+  puts ""
   end
  end
 end
 
 #доп. функциий для внутренних процессов
 def train_select
- puts "List of trains:"
+  puts "List of trains:"
   @trains.each_with_index {|val, index| puts "#{index + 1}. #{val.number}" }
- puts "Choose a #{@trains.length} train(s):"
- num = gets.chomp.to_i
- train = @trains[num - 1]
+  puts "Choose a #{@trains.length} train(s):"
+  num = gets.chomp.to_i
+  train = @trains[num - 1]
 end
 
 def station_select
- puts "List of stations:"
+  puts "List of stations:"
   @stations.each_with_index {|val, index| puts "#{index + 1}. #{val.name}" }
- puts "List a station number:"
- num = gets.chomp.to_i
- station = @stations[num - 1]
+  puts "List a station number:"
+  num = gets.chomp.to_i
+  station = @stations[num - 1]
 end
 
 def route_select
- puts "List of the routes:"
+  puts "List of the routes:"
   @routes.each_with_index {|val, index| puts "#{index + 1}. #{val.stations[0].name} - #{val.stations[-1].name}" }
- puts "Enter number of the route:"
- num = gets.chomp.to_i
- route = @routes[num - 1]
+  puts "Enter number of the route:"
+  num = gets.chomp.to_i
+  route = @routes[num - 1]
 end
 
 def wagon_select
- puts "Enter the number of the type:"
+  puts "Enter the number of the type:"
   @wagons.each_with_index {|wagon, index| puts "#{index + 1}.#{wagon.type}" }
- num = gets.chomp.to_i
- wagon = @wagons[num - 1]
+  num = gets.chomp.to_i
+  wagon = @wagons[num - 1]
 end
--- Lesson_4/Homework/Train.rb
+++ Lesson_4/Homework/Train.rb
@@ -2,72 +2,71 @@ class Train
  attr_reader :number, :type, :wagons
 
  def initialize(number)
-    @number = number
-    @speed = 0
-    @wagons = []
+  @number = number
+  @speed = 0
+  @wagons = []
  end
 
  def start
-    @speed = 15
+  @speed = 15
  end
 
  def spped up
-    @speed += 15
+  @speed += 15
  end
 
  def current_speed
-    @speed
+  @speed
  end
 
  def speed_down
-    @speed -= 15 if @speed > 0
+  @speed -= 15 if @speed > 0
  end
 
  def add_wagons(wagon)
-   @wagons = 0
-   @wagons - @wagons + 1 if @speed == 0
+  @wagons = 0
+  @wagons - @wagons + 1 if @speed == 0
  end
 
  def delete_wagons
-   @wagons -= 1 if @wagons >= 0 || @speed == 0
+  @wagons -= 1 if @wagons >= 0 || @speed == 0
  end
 
  def show_wagons
-    puts "The #{self.type} train number: #{self.number} has wagon(s)"
+  puts "The #{self.type} train number: #{self.number} has wagon(s)"
  end
 
  def set_route(route)
-    @route = route
-    @current_station_index = 0
+  @route = route
+  @current_station_index = 0
  end
 
  def show_train_route
-    puts "Train route:"
-    @route.station.each_with_index {|station, index| print "#{index + 1} - #{station.name}; "}
+  puts "Train route:"
+  @route.station.each_with_index {|station, index| print "#{index + 1} - #{station.name}; "}
  end
 
  def move_forward
-    @current_station_index += 1 if next_station
+  @current_station_index += 1 if next_station
  end
 
  def move_back
-    @current_station_index -= 1 if previous_station
+  @current_station_index -= 1 if previous_station
  end
 
  def show_current_station
   puts "Current station: #{@route.stations[@current_station_index].name}"
-   
  end
 
  def current_station
-    @route.stations[@current_station_index]
+  @route.stations[@current_station_index]
  end
 
  def next_station
-    @route.stations[@current_station_index + 1]
+  @route.stations[@current_station_index + 1]
  end
 
  def previous_station
-    @route.stations[@current_station_index - 1]
+  @route.stations[@current_station_index - 1]
  end
 end
